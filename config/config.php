28edcb3c10514925c30b53edf79b50cbb2aedef6
Saad Nasir <sa>
2018-04-05 02:38:32 +0500

    fix config

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'api_url' => env('PUBG_API_URL', 'https://api.playbattlegrounds.com/shards/'),
-    'token'   => env('PUBG_ACCESS_TOKEN'),
+    'access_token'   => env('PUBG_ACCESS_TOKEN'),
 ];
