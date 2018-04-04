58ee5abd1b2dd2734b35f2bfa701ca12c9ffbb12
Saad Nasir <sa>
2018-04-05 03:15:37 +0500

    add default region

--- config/config.php
+++ config/config.php
@@ -2,5 +2,5 @@
 return [
     'api_url' => env('PUBG_API_URL', 'https://api.playbattlegrounds.com/shards/'),
     'access_token'   => env('PUBG_ACCESS_TOKEN'),
-    'region'   => env('PUBG_REGION'),
+    'region'   => env('PUBG_REGION', 'pc-eu'),
 ];
